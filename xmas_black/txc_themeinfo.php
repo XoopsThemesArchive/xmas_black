1f184a4ea8b51d0bc1ee7d1fc96f4dc528e3526d
mambax7 <mambax7@gmail.com>
2015-05-05 09:39:23 -0400

    Original files

---
+++ xmas_black/txc_themeinfo.php
@@ -0,0 +1,5 @@
+<?php
+	$txc_themeinfo["name"] = "xmas_black";
+	$txc_themeinfo["author"] = "ImageSquare";
+	$txc_themeinfo["detail_url"] = "http://xoops.org.cn/modules/wfdownloads/visit.php?cid=6&lid=161";
+?>
